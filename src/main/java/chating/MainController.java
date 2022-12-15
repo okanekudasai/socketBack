bd2c9ea933a9109e21712d3e467a05305170fdc7
opi6opi6 <opi6@hanmail.net>
2022-12-15 13:26:35 +0900

    second

--- src/main/java/chating/MainController.java
+++ src/main/java/chating/MainController.java
@@ -2,8 +2,10 @@ package chating;
 
 import java.util.HashMap;
 import java.util.LinkedList;
+import java.util.Map;
 import java.util.UUID;
 
+import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
@@ -12,6 +14,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+
 
 
 @Controller
@@ -38,16 +42,55 @@ public class MainController {
 				.build();
 		chatingRoom.getUsers().add(map.get("userId"));
 		chatingRoomList.add(chatingRoom);
-		System.out.println(roomNumber);
 		return roomNumber;
 	}
 	
 	//채팅룸 넘버를 가지고 채팅룸 인스턴스를 찾아요
+	@PostMapping("/findRoom")
+	@ResponseBody
+	public ChatingRoom findRoomByNumber(@RequestBody String roomNumber) {
+		ChatingRoom room = ChatingRoom.builder().roomNumber(roomNumber).build(); 
+		int index = chatingRoomList.indexOf(room);
+		
+		if(chatingRoomList.contains(room)) {
+			return chatingRoomList.get(index); 
+		}
+		return null;
+	}
 	
+	@PostMapping("/enterChat")
+	@ResponseBody
+	public String enterChat(@RequestBody HashMap<String, String> map) {
+		ChatingRoom room = ChatingRoom.builder().roomNumber(map.get("roomNumber")).build(); 
+		int index = chatingRoomList.indexOf(room);
+		
+		ChatingRoom thisRoom = chatingRoomList.get(index);
+		thisRoom.getUsers().add(map.get("userId"));
+		
+		return thisRoom.getRoomName();
+	}
 	
 	@MessageMapping("/socket/roomList")
 	@SendTo("/topic/roomList")
-	public void roomList() {
-		System.out.println("룸리스트를 업데이트해요");
+	public String roomList() {
+//		룸리스트를 업데이트해요
+//		리턴을 null하거나 void함수로 만들면 subscribe가 아무것도 잡지 못하는거 같아요
+		return "";
+	}
+	
+	// 채팅방에서 메세지 보내기
+	@MessageMapping("/socket/sendMessage/{roomNumber}")
+	@SendTo("/topic/message/{roomNumber}")
+	public Message sendMessage(@DestinationVariable String roomNumber, Message mapa) {
+		System.out.println(roomNumber);
+		System.out.println(mapa);
+		return mapa;
+	}
+	
+	// 채팅방에 입장 퇴장 메세지 보내기
+	@MessageMapping("/socket/notification/{roomNumber}")
+	@SendTo("/topic/notification/{roomNumber}")
+	public Map<String, String> notification(@DestinationVariable String roomNumber, Map<String, String> chatingRoom) {
+		return chatingRoom;
 	}
 }
